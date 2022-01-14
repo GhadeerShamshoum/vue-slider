0849725b3f294149678f363186202e4637d113bc
Ghadeer <ghaora@gmail.com>
2022-01-14 17:48:38 +0200

    array di 5 oggetti

--- myscript.js
+++ myscript.js
@@ -3,43 +3,53 @@ let app = new Vue({
     data: {
         
         present: 0,
-        items:{
+        items:[
 
-            photos: [
-                'img/01.jpg',
-                'img/02.jpg',
-                'img/03.jpg',
-                'img/04.jpg',
-                'img/05.jpg'
-            ],
-            title: [
-                'Svezia',
-                'Svizzera',
-                'Gran Bretagna',
-                'Germania',
-                'Paradise'
-            ],
-            text: [
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                'Lorem ipsum',
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-                'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-            ]
+            {
+                photo: 'img/01.jpg',
+                title:'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',   
+                
+            },
+            {
+                photo: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum',
+            }, 
+            {
+                photo: 'img/03.jpg',
+                title:  'Gran Bretagna',
+                text:  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', 
+            },
+
+            {
+                photo:'img/04.jpg',
+                title:'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+          
+            },
+            {
+                
+               photo:'img/05.jpg',
+               title:'Paradise',
+               text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',  
+                
+            },
             
-        }
+            
+        ]
     },
     methods:{
-        isPresent: function( indexPhotos){
-            if(indexPhotos == this.present){
+        isPresent: function( indexPhoto){
+            if(indexPhoto == this.present){
                 return "active";
             }
             return "";
             
         },
-        sidePresent: function( indexPhotos){
-            console.log(indexPhotos, this.present)
-            if(indexPhotos == this.present){
+        sidePresent: function( indexPhoto){
+            console.log(indexPhoto, this.present)
+            if(indexPhoto == this.present){
                 return "opacity";
             }
             return "";
