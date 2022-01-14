9ebd4e2f801ee78563a8cc7ab460efe9e941eaee
Ghadeer <ghaora@gmail.com>
2022-01-14 17:01:14 +0200

    tutti gli array in un object

--- myscript.js
+++ myscript.js
@@ -3,36 +3,39 @@ let app = new Vue({
     data: {
         
         present: 0,
-        items: [
-            'img/01.jpg',
-            'img/02.jpg',
-            'img/03.jpg',
-            'img/04.jpg',
-            'img/05.jpg'
-        ],
-        title: [
-            'Svezia',
-            'Svizzera',
-            'Gran Bretagna',
-            'Germania',
-            'Paradise'
-        ],
-        text: [
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-            'Lorem ipsum',
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-            'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-        ]
-        
+        items:{
+
+            photos: [
+                'img/01.jpg',
+                'img/02.jpg',
+                'img/03.jpg',
+                'img/04.jpg',
+                'img/05.jpg'
+            ],
+            title: [
+                'Svezia',
+                'Svizzera',
+                'Gran Bretagna',
+                'Germania',
+                'Paradise'
+            ],
+            text: [
+                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                'Lorem ipsum',
+                'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+                'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            ]
+            
+        }
     },
     methods:{
         isPresent: function( indexPhotos){
-            console.log(indexPhotos, this.present)
             if(indexPhotos == this.present){
                 return "active";
             }
             return "";
+            
         },
         sidePresent: function( indexPhotos){
             console.log(indexPhotos, this.present)
